21a57531e8d760e29a171681ce5f8c35a01f7325
ido <idoronen101@gmail.com>
2019-08-04 16:36:56 +0300

    Updates

---
+++ precache-manifest.fa8d3682d1c13a11bdbdc41a7b562ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc38fa906de8a80b534d04101077225f",
+    "url": "/React-proj/index.html"
+  },
+  {
+    "revision": "34bd424347f8bf787599",
+    "url": "/React-proj/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "56104d27b235ca3baff1",
+    "url": "/React-proj/static/js/2.d2702295.chunk.js"
+  },
+  {
+    "revision": "34bd424347f8bf787599",
+    "url": "/React-proj/static/js/main.a37e40b1.chunk.js"
+  },
+  {
+    "revision": "7ec04fccf454ac2f2f57",
+    "url": "/React-proj/static/js/runtime~main.90801192.js"
+  }
+]);
